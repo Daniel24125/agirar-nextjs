cf5bcacc52b532352b7cd684b796f378e8c9b5cd
Daniel24125 <danielmadalena@msn.com>
2023-10-16 15:22:35 +0100

    creating the project

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Agirar</title>
+        <meta name="description" content="Associação de Familiares e Amigos de Pessoas com Psicose" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        
+      </main>
+    </>
+  )
+}
