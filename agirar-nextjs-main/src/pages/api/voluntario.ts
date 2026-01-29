441af97f7c934d7ffec32567079c8722ff1f3799
Daniel24125 <danielmadalena@msn.com>
2026-01-29 20:24:57 +0000

    feat: Add API routes for handling new member contributions and volunteer applications with email notifications.

--- agirar-nextjs-main/src/pages/api/voluntario.ts
+++ agirar-nextjs-main/src/pages/api/voluntario.ts
@@ -1,32 +1,30 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { newMemberTemplate, newVolunteerTemplate, sendEmail } from '@/utils/ServerUtils'
+import { newVolunteerTemplate, sendEmail } from '@/utils/ServerUtils'
 import type { NextApiRequest, NextApiResponse } from 'next'
-const {MailtrapClient} = require("mailtrap")
 
 type Data = {
   msg?: string
   error: boolean
 }
 
-const client = new MailtrapClient({token: process.env.TOKEN})
 
-export default async function handler (
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const {body, method} = req
+  const { body, method } = req
   switch (method) {
     case "POST":
-      const {email} = body
-      const response = await sendEmail(email, "Pedido de Voluntariado", newVolunteerTemplate(body))
+      const { email } = body
+      const response = await sendEmail(["agirar.2013@gmail.com"], "Pedido de Voluntariado", newVolunteerTemplate(body))
       res.json({
         error: false,
         msg: "Muito obrigado pela sua contribuição! Cada gesto vale a pena! Iremos certamente entrar em contacto consigo brevemente. 😃"
       })
       break;
-  
+
     default:
-      res.status(404).json({error: true, msg: "O endpoit que tentou aceder não está disponível."})
+      res.status(404).json({ error: true, msg: "O endpoit que tentou aceder não está disponível." })
       break;
   }
 }
